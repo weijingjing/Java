e579fffdfb22b8d698024909f9071114570a4589
weijingjing <649288267@qq.com>
2017-09-08 17:31:48 +0800

    test9.8

---
+++ src/main/java/com/jing/list/SetTest.java
@@ -0,0 +1,7 @@
+package com.jing.list;
+
+/**
+ * Created by weijj on 2017/9/8.
+ */
+public class SetTest {
+}
---
+++ src/main/java/com/jing/list/Student.java
@@ -0,0 +1,10 @@
+package com.jing.list;
+
+/**
+ * Created by weijj on 2017/9/8.
+ */
+public class Student {
+    public int id;
+    public String name;
+    public Course
+}
